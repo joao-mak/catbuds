57c72ce5f4d18c33312902021d4d7b1ffc009234
joao-mak <joao_mak@msn.com>
2020-07-30 15:22:31 +0100

    Updates

---
+++ precache-manifest.94408b371b346e0888262051a2107c5a.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "9d7c5e20e1b3790b79d17f170f4547da",
+    "url": "/catbuds/index.html"
+  },
+  {
+    "revision": "be40bc320c3d39dd5bd4",
+    "url": "/catbuds/static/css/2.3c03f26f.chunk.css"
+  },
+  {
+    "revision": "4870c5ac4bd664a4755d",
+    "url": "/catbuds/static/css/main.dc35c7dd.chunk.css"
+  },
+  {
+    "revision": "be40bc320c3d39dd5bd4",
+    "url": "/catbuds/static/js/2.863f4aec.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/catbuds/static/js/2.863f4aec.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "4870c5ac4bd664a4755d",
+    "url": "/catbuds/static/js/main.ce0a4842.chunk.js"
+  },
+  {
+    "revision": "a32601ca1b841e5fc164",
+    "url": "/catbuds/static/js/runtime-main.55c5eac6.js"
+  }
+]);
